795e51603183fb3f3821bd6eb4df364fdd179eeb
Sang-Jin <lee3jjang@gmail.com>
2021-08-15 21:34:36 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'App.css'
+import Header from 'components/Header'
+import Contents from 'components/Contents'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Contents />
+    </div>
+  )
+}
+
+export default App
